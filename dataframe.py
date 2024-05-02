347b53c2cec34a8b31f5603ead9c573849a8ae49
leesj0706 <leesj9627@naver.com>
2024-05-02 17:54:14 +0900

    Upload

--- dataframe.py
+++ dataframe.py
@@ -6,35 +6,36 @@ import seaborn as sns
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("gapminder.tsv", sep="\t") # 외부 데이터
+    df = pd.read_csv("gapminder.tsv", sep="\t") # 외부 텍스트 데이터  -> 데이터프레임 반환
     return df
 
 def plot_matplotlib():
-    st.title("Categorical Bar Plot with Seaborn")
-    df = load_data()
+    st.title(" Bar Plot")
+    df = load_data() # 데이터 준비
+    # 그래프 그리기
     fig, ax = plt.subplots()
     
-    # Using Seaborn's barplot function
+    # 막대 그래프 
     sns.barplot(x=df['year'], y=df['lifeExp'], data=df, ax=ax)
     
     # Labeling axes and title
     ax.set_xlabel("year")
     ax.set_ylabel("lifeExp")
-    ax.set_title("Bar Plot : Year vs. lifeExp")
-    
+    ax.set_title("Year vs. lifeExp")
+    # 웹 앱 시각화
     st.pyplot(fig)
 
 def main():
-    st.title("데이터 시각화 : 표 & 그래프")
- 
-    df = load_data() # 데이터 불러오기
-    st.dataframe(df, use_container_width=True)
+    st.title("데이터 시각화 : 표  & 그래프")
+    
+    df = load_data() # 데이터 불러오기 : 데이터프레임
+    st.dataframe(df, use_container_width=True) # 첫번째 표
 
-    #pandas style
+    #두번째 표
     st.title("컬럼별 최대값 표")
     st.dataframe(df.iloc[:5,2:].style.highlight_max(axis=0))
 
-    plot_matplotlib()
+    plot_matplotlib() # 그래프
     
     
 if __name__ == "__main__":
